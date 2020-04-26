a6f5eb804279880462837149fde3980e5e70c3cd
JooYoung <sana2dang@naver.com>
2020-04-26 18:10:31 +0900

    스크롤링 스피드 픽스
    롱 제목 리스트 좌우 스크롤링 스피드 픽스

--- es-app/src/components/TextListComponent.h
+++ es-app/src/components/TextListComponent.h
@@ -315,8 +315,8 @@ void TextListComponent<T>::update(int deltaTime)
 			// loop
 			// pixels per second ( based on nes-mini font at 1920x1080 to produce a speed of 200 )
 			//const float speed        = mFont->sizeText("ABCDEFGHIJKLMNOPQRSTUVWXYZ").x() * 0.500f;
-			const float speed        = 150;
-			const float delay        = 500;
+			const float speed        = 130;
+			const float delay        = 1000;
 			const float scrollLength = textLength;
 			const float returnLength = speed * 1.5f;
 			const float scrollTime   = (scrollLength * 1000) / speed;
